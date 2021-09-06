67d673a6e1a0d0d78c7dbee8e8c469e7000683f9
Scott Bilas <scottbilas@gmail.com>
2021-09-06 23:16:50 +0200

    More migration into cm

--- Documents/exact_PowerShell/profile.ps1
+++ Documents/exact_PowerShell/profile.ps1
@@ -4,23 +4,24 @@ $ErrorActionPreference = 'Stop'
 # important: don't set strict mode for the profile. rando cli ops need to stay fuzzy else they get annoying.
 # but stopping on any error is good, let's keep that one.
 
+
 ### POSH CORE
 
 # override the worst default ever (UTF-16LE); see much detail at https://stackoverflow.com/a/40098904/14582
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8NoBOM'
 
+
 ### ENVIRONMENT
 
+$Env:BAT_CONFIG_PATH = "$HOME/.config/bat/bat.conf"
+$Env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+$Env:FZF_DEFAULT_COMMAND = 'fd --hidden -E .git'
+$Env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview-window=right:60% --bind 'alt-p:toggle-preview' --preview '$HOME\dotfiles\scripts\fzf-preview.cmd {} | head -500'"
 $Env:HOME = Resolve-Path ~
 $Env:LESS = '--tabs=4 -RFXi'
-$Env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview-window=right:60% --bind 'alt-p:toggle-preview' --preview '$HOME\dotfiles\scripts\fzf-preview.cmd {} | head -500'"
-$Env:FZF_DEFAULT_COMMAND = 'fd --hidden -E .git'
-$Env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+$Env:RIPGREP_CONFIG_PATH = (Resolve-Path ~/.config/ripgrep/config)
+$Env:SCOOP = Resolve-Path ~\scoop
 $Env:UNITY_MIXED_CALLSTACK = 1
-
-# redirecting defaults out of root
-$Env:RIPGREP_CONFIG_PATH = (resolve-path ~/.config/ripgrep/config)
-$Env:BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"
 
 
 ### PSREADLINE
@@ -33,6 +34,7 @@ Set-PSReadLineOption `
     -HistorySearchCursorMovesToEnd `
     -PredictionSource History `
     -PredictionViewStyle ListView
+
 
 ### ALIASES AND ALIAS-ISHES
 
@@ -54,7 +56,7 @@ function ov($what) { Set-Location ../$what }
 function ~ { Set-Location ~ }
 
 # default sort.exe is in system32; avoid that one
-&{
+& {
     $sort = Resolve-Path ~\scoop\apps\git\current\usr\bin\sort.exe
     if (Test-Path $sort) {
         Set-Alias -Scope Global sort $sort
@@ -62,9 +64,10 @@ function ~ { Set-Location ~ }
 }
 
 Set-Alias psdev (Join-Path $PSScriptRoot dev.ps1)
-Set-Alias psup (Join-Path $PSScriptRoot setup.ps1) # TODO: have it also call install.ps1sh
+Set-Alias pssetup (Join-Path $PSScriptRoot setup.ps1)
+function psup { & (Join-Path $PSScriptRoot setup.ps1) -Upgrade }
 
-&{
+& {
     $bc = 'C:\Program Files\Beyond Compare 4\BComp.exe'
     if (Test-Path $bc) {
         Set-Alias -Scope Global bc $bc
@@ -73,4 +76,3 @@ Set-Alias psup (Join-Path $PSScriptRoot setup.ps1) # TODO: have it also call ins
 
 
 ### FUNCTIONS
-
--- Documents/exact_PowerShell/setup.ps1
+++ Documents/exact_PowerShell/setup.ps1
@@ -1,11 +1,12 @@
 #Requires -Version 7
+
+[CmdletBinding()]
+param (
+    [switch]$Upgrade
+)
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
-
-# TODO: make param
-$Upgrade = $false
-
-# UPGRADES
 
 ## Chezmoi
 
@@ -16,10 +17,11 @@ $Upgrade = $false
 # ! this must come before we pull in modules
 
 if ((Get-PSRepository PSGallery).InstallationPolicy -ne 'Trusted') {
-    Write-Output "[posh] Trusting PSGallery..."
+    Write-Output '[posh] Trusting PSGallery...'
     Set-PSRepository PSGallery -InstallationPolicy Trusted
-} else {
-    Write-Output "[posh] PSGallery ok"
+}
+else {
+    Write-Output '[posh] PSGallery ok'
 }
 
 ## PSDepend
@@ -38,30 +40,114 @@ if (!$invokePSDepend) {
 }
 
 if ($invokePSDepend) {
-    Write-Output "[posh] Pulling modules..."
+    Write-Output '[posh] Pulling modules...'
     Invoke-PSDepend -Force $PSScriptRoot
-} else {
-    Write-Output "[posh] Modules ok"
+}
+else {
+    Write-Output '[posh] Modules ok'
 }
 
-## Scoop
+## Scoop pre
 
 if ((iee scoop config show_update_log) -match 'not set') {
     iee scoop config show_update_log false
-} else {
-    Write-Output "[scoop] Config ok"
+}
+else {
+    Write-Output '[scoop] Config ok'
 }
 
 ## Shovel
 
 if ((scoop config SCOOP_REPO) -ne 'https://github.com/Ash258/Scoop-Core') {
-    Write-Output "[scoop] Upgrading to Shovel..."
+    Write-Output '[scoop] Upgrading to Shovel...'
     iee scoop config SCOOP_REPO https://github.com/Ash258/Scoop-Core
     iee scoop config SCOOP_BRANCH main
     iee scoop update
-} else {
-    Write-Output "[scoop] Shovel ok"
 }
+else {
+    Write-Output '[scoop] Shovel ok'
+}
+
+## Scoop buckets
+
+if ($Upgrade) {
+    Write-Output '[scoop] Updating buckets'
+    iee scoop update
+}
+
+$buckets = iee scoop bucket
+
+function addBucket([string]$name, [string]$url = $null) {
+    if ($buckets -notcontains $name) { iee scoop bucket add $name $url }
+}
+
+addBucket extras
+addBucket github-gh https://github.com/cli/scoop-gh.git
+addBucket nerd-fonts
+addBucket nirsoft
+addBucket nonportable
+addBucket twpayne https://github.com/twpayne/scoop-bucket
+
+Write-Output '[scoop] Bucket presence ok'
+
+## Scoop core
+
+$packages = (Get-ChildItem ~/scoop/apps).Name
+
+function addPackage([string]$name, [switch]$sudo) {
+    if ($packages -notcontains $install) {
+        if ($sudo) {
+            sudo ie scoop install $install
+        }
+        else {
+            ie scoop install $install
+        }
+    }
+}
+
+foreach ($install in @(
+        # stuff shovel wants
+        'dark', 'innounp', 'lessmsi'
+        # shell stuff
+        'busybox', 'less', 'which', 'echoargs', 'wget', 'lz4'
+        # other core utils
+        'ripgrep', 'kalk', '7zip', 'autohotkey', 'fd', 'gsudo', 'chezmoi', 'git'
+    )) {
+    addPackage($install)
+}
+
+# special note on fonts: avoid packages with "mono" in the name - these lose ligatures ("nerd font policy")
+addPackage -sudo CascadiaCode-NF
+addPackage -sudo JetBrainsMono-NF
+
+<#
+# each of these requires special setup
+
+barrier-np
+everything
+gh
+syncthingtray
+
+7tt
+linqpad
+micro
+nodejs
+paint.net
+sysinternals
+tailblazer
+vscode-portable
+vcxsrv
+windirstat
+
+perl
+python
+go
+rust
+
+unity-downloader-cli
+#>
+
+Write-Output '[scoop] Core packages ok'
 
 # CHECKS
 
@@ -70,6 +156,11 @@ $Env:PATH = ($Env:PATH -Split ';' | Where-Object { $_ }) -join ';' # fix any bla
 $badPaths = ($Env:PATH).Split(';') | Where-Object { !(Test-Path ([Environment]::ExpandEnvironmentVariables($_))) }
 if ($badPaths) {
     Write-Error "PATH is invalid: $(($badPaths | ForEach-Object { "'$_'" }) -Join ', ')"
-} else {
-    Write-Output "[scoop] PATH ok"
+}
+else {
+    Write-Output '[scoop] PATH ok'
+}
+
+if ($Upgrade) {
+    scoop status
 }
