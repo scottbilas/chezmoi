fff12ff87348bc36fb13880d2cf3f41bb4475431
Scott Bilas <scottbilas@gmail.com>
2023-09-20 14:00:57 +0200

    One more to ignore

--- home/Documents/exact_PowerShell/profile.ps1
+++ home/Documents/exact_PowerShell/profile.ps1
@@ -68,7 +68,7 @@ Set-PSReadLineOption `
 ### ALIASES AND ALIAS-ISHES
 
 # these aliases only cause problems or collide with other things or otherwise are lame
-'diff', 'rm', 'set', 'sort', 'r', 'kill' | ForEach-Object {
+'diff', 'rm', 'set', 'sort', 'r', 'kill', 'tee' | ForEach-Object {
     # remove from every scope (https://stackoverflow.com/a/24743647/14582)
     while (Test-Path Alias:$_) {
         Remove-Item -Force Alias:$_
