4f263b7134a52d6ec560fca0ab5344f6b204a2bf
Scott Bilas <scottbilas@gmail.com>
2023-10-04 20:01:33 +0200

    More git worktree fun

--- home/Documents/exact_PowerShell/exact_Modules/exact_scobi/exact_Public/Git.ps1
+++ home/Documents/exact_PowerShell/exact_Modules/exact_scobi/exact_Public/Git.ps1
@@ -1,3 +1,7 @@
+[Diagnostics.CodeAnalysis.SuppressMessage('PSUseApprovedVerbs', '')]
+[Diagnostics.CodeAnalysis.SuppressMessage('PSPossibleIncorrectUsageOfRedirectionOperator', '')]
+param()
+
 <# WIP WIP
 function Git-GetChangedFiles {
     [CmdletBinding()]
@@ -58,7 +62,7 @@ function Git-LsTree {
 }
 Export-ModuleMember Git-LsTree
 
-filter Git-ToDict($Delim = ':') {
+filter Git-ToDict([string]$Delim = ':') {
     begin {
         $dict = @{}
     }
@@ -80,26 +84,102 @@ filter Git-ToDict($Delim = ':') {
 }
 Export-ModuleMember Git-ToDict
 
-function Git-WorktreeList {
-    [CmdletBinding(PositionalBinding=$false)]
+function Git-WorktreeList([string]$RepoRoot = $null) { # corresponds to git -C
+
+    $gitArgs = @()
+    if ($RepoRoot) {
+        $gitArgs += '-C', $RepoRoot
+    }
+    else {
+        $RepoRoot = '.'
+    }
+    $gitArgs += 'worktree', 'list', '--porcelain'
+
+    foreach ($wt in git $gitArgs | Git-ToDict -Delim ' ') {
+        if ($wt.ContainsKey('branch')) {
+            if ($wt.branch.StartsWith("refs/heads/")) {
+                $wt.branch = $wt.branch.Substring(11)
+            }
+        }
+        else {
+            $wt.branch = $null
+        }
+
+        $wt.detached = $wt.ContainsKey('detached')
+
+        $wt.gitdir = Resolve-Path ($wt.worktree + '/.git')
+        $wt.worktree = Resolve-Path $wt.worktree
+
+        if (Test-Path -PathType Leaf $wt.gitdir) {
+            $gd = (Get-Content $wt.gitdir | Git-ToDict).gitdir
+            $wt.worktree_gitdir_raw = $gd
+
+            if (![IO.Path]::IsPathRooted($gd)) {
+                $gd = Join-Path $RepoRoot $gd
+            }
+
+            $wt.worktree_gitdir = Resolve-Path $gd
+
+            $wt.gitconfig = Resolve-Path (Join-Path $wt.worktree_gitdir .. .. config)
+            $wt.worktree_gitconfig = $wt.worktree_gitdir
+        }
+        else {
+            $wt.gitconfig = Resolve-Path (Join-Path $wt.gitdir config)
+            $wt.worktree_gitconfig = $wt.gitdir
+        }
+
+        $wt.worktree_gitconfig = Resolve-Path (Join-Path $wt.worktree_gitconfig config.worktree)
+
+        $wt
+    }
+}
+Export-ModuleMember Git-WorktreeList
+
+function Git-FixConfigs {
+    [CmdletBinding(SupportsShouldProcess)]
     param (
         [string]$RepoRoot = $null # corresponds to git -C
     )
 
-    $gitArgs = @()
-    if ($RepoRoot) { $gitArgs += '-C', $RepoRoot }
-    $gitArgs += 'worktree', 'list', '--porcelain'
+    # TODO: also check for and include.path if exists in sync folder (remove if not..?)
 
-    git $gitArgs | Git-ToDict -Delim ' ' | ForEach-Object {
-        $gitdir = $_.worktree + '/.git'
-        $_.gitdir = $gitdir
-        if (Test-Path -PathType Leaf $_.gitdir) {
-            $_.gitdirwt = (Get-Content $_.gitdir | Git-ToDict).gitdir
+    foreach ($wt in Git-WorktreeList -RepoRoot $RepoRoot) {
+        if ($wt.ContainsKey('worktree_gitdir_raw') -and
+            [IO.Path]::IsPathRooted($wt.worktree_gitdir_raw) -and
+            $PSCmdlet.ShouldProcess($wt.gitdir, 'Fix .git path to be relative')) {
+
+            Write-Host "Fixing gitdir path to be relative in $($wt.gitdir)"
+            
+            $rel = [IO.Path]::GetRelativePath($wt.worktree, $wt.worktree_gitdir).Replace('\', '/')
+            $old = [IO.File]::ReadAllText($wt.gitdir)
+            $new = $old -replace 'gitdir:\s*?.*', "gitdir: $rel"
+            if ($old -ne $new) {
+                [IO.File]::WriteAllText($wt.gitdir, $new)
+            }
+            else {
+                Write-Error "Pattern match fail in $($wt.gitdir)"
+            }
         }
-        if ($_.branch.StartsWith("refs/heads/")) {
-            $_.branch = $_.branch.Substring(11)
+
+        if ($wt.ContainsKey('worktree_gitconfig')) {
+            $wtconfig = Get-IniContent $wt.worktree_gitconfig
+            $include = $wtconfig['include']?['path']
+            if ($include -and
+                [IO.Path]::IsPathRooted($include) -and
+                $PSCmdlet.ShouldProcess($wt.worktree_gitconfig, 'Fix include.path to be relative')) {
+
+                Write-Host "Fixing include.path to be relative in $($wt.worktree_gitconfig)"
+
+                $rel = [IO.Path]::GetRelativePath($wt.worktree, $include).Replace('\', '/')
+                $wtconfig['include']['path'] = $rel
+                $wtconfig | Out-IniFile $wt.worktree_gitconfig -force -loose -pretty
+            }
         }
-        $_
+
+        # test the worktree is ok
+        if ((git -C $wt.worktree rev-parse --is-inside-work-tree) -ne 'true') {
+            Write-Error "Worktree $($wt.worktree) is not ok"
+        }
     }
 }
-Export-ModuleMember Git-WorktreeList
+Export-ModuleMember Git-FixConfigs
