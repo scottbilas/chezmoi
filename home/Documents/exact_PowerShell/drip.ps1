fb876c71130297ad1d206d442b808e2361f288d1
Scott Bilas <scottbilas@gmail.com>
2025-01-17 17:53:37 +0000

    tweak

--- home/Documents/exact_PowerShell/drip.ps1
+++ home/Documents/exact_PowerShell/drip.ps1
@@ -1,21 +1,30 @@
-foreach ($service in (get-service `
-    *razer*, `
-    RzActionSvc, `
-    *nvidia*, `
-    HPPrintScan*, `
-    Mgl3DCtlrRPCService, `
-    Samsung*, `
-    ZeroTier*, `
-    Epic*, `
-    GameSDK*, `
-    JetBrainsEtwHost*)) {
+foreach ($service in (get-service -ea:silent `
+    *razer*,
+    RzActionSvc,
+    *nvidia*,
+    HPPrintScan*,
+    Mgl3DCtlrRPCService,
+    Samsung*,
+    ZeroTier*,
+    Epic*,
+    GameSDK*,
+    JetBrainsEtwHost*,
+    DSAUpdateService,
+    DSAService)) {
     if ($service.Status -ne 'Running') { continue }
     "Stopping service: $($service.Name)"
     $service | stop-service -Force -ea:Continue
 }
 
-foreach ($process in (get-process `
-    nvidia*)) {
+foreach ($process in (get-process -ea:Silent `
+    nvidia*,
+    onedrive,
+    syncthingtray,
+    *resilio*,
+    ferdium,
+    dsatray,
+    zerotier_desktop_ui
+    )) {
     "Stopping process: $($process.Name)"
     $process | stop-process -Force -ea:Continue
 }
