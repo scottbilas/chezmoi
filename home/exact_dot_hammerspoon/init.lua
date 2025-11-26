54304d22c8bc6a940cecc8d67e9bede97fed0242
Scott Bilas <scottbilas@gmail.com>
2025-11-26 13:02:31 +0000

    Just switch to my own version and fix it up

--- home/exact_dot_hammerspoon/init.lua
+++ home/exact_dot_hammerspoon/init.lua
@@ -1,13 +1,19 @@
+-- easy window resize/move
 local SkyRocket = hs.loadSpoon("SkyRocket")
-
 sky = SkyRocket:new({
   opacity = 0.5,
 
+  -- printWindowInfo = true,  -- uncomment to debug window properties
+
+  -- ignore the green zoom frame (during partial screen share)
+  disabledApps = {
+    { bundleID = 'us.zoom.xos', title = 'Annotation - Zoom', action = 'passthrough' },
+  },
+
   moveModifiers = {'cmd', 'alt'},
   moveMouseButton = 'left',
-
   resizeModifiers = {'cmd', 'shift'},
   resizeMouseButton = 'left',
-
-  focusWindowOnClick = false,
+  regionRatio = 0.25,
+  focusWindowOnClick = true,
 })
