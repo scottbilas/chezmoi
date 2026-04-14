61c46fa7c8885b132dce31c7739d5267d27564ab
Scott Bilas <scottbilas@gmail.com>
2026-04-14 05:25:06 +0100

    Fix for naming change

--- home/Documents/exact_PowerShell/profile.ps1
+++ home/Documents/exact_PowerShell/profile.ps1
@@ -118,7 +118,7 @@ function Get-ConfigToml {
 
     $path = Join-Path $baseDir scott.toml
     if (!(Test-Path $path)) {
-        $path = Join-Path $baseDir scobly.toml
+        $path = Join-Path $baseDir scobely.toml
     }
 
     Get-Content $path | convertfrom-toml
